c3bb407500c137fdf15a73c503a09d628a15a525
Jefferson Felix <jefferson.felix182@gmail.com>
2022-12-14 17:25:13 -0300

    req 2 - id do request.params

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_FAIL_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -20,6 +21,20 @@ app.get('/talker', async (_request, response) => {
     return response.status(HTTP_OK_STATUS).send([]);
   }
   return response.status(HTTP_OK_STATUS).send(db);
+});
+
+// req 2
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const db = await readJson();
+    const filterById = db.find((element) => element.id === Number(id));
+    if (!filterById) {
+      return response.status(HTTP_FAIL_STATUS).send({
+        message: 'Pessoa palestrante não encontrada',
+      });
+    }
+    return response.status(HTTP_OK_STATUS).send(filterById);
 });
 
 app.listen(PORT, () => {
