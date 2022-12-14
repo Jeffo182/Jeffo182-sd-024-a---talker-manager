ded2ca6c26d266e7694e7423d24e55597272e09e
Jefferson Felix <jefferson.felix182@gmail.com>
2022-12-14 17:08:23 -0300

    fix: condicao array vazio

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_request, response) => {
   const db = await readJson();
-  response.status(HTTP_OK_STATUS).send(db);
+  if (db.length === 0) {
+    return response.status(HTTP_OK_STATUS).send([]);
+  }
+  return response.status(HTTP_OK_STATUS).send(db);
 });
 
 app.listen(PORT, () => {
