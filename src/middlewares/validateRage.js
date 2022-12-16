ac8abf66df7f148cf912fc338afdba0721a4803d
Jefferson Felix <jefferson.felix182@gmail.com>
2022-12-15 21:19:20 -0300

    fim do dia, terminar req 6

--- src/middlewares/validateRage.js
+++ src/middlewares/validateRage.js
@@ -1,15 +1,11 @@
-const validateRate = (req, res, next) => {
+const validateRage = (req, res, next) => {
   const {
     talk: { rate },
   } = req.body;
   if (!rate) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
-  if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
-    return res
-      .status(400)
-      .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
+
   next();
 };
-module.exports = validateRate;
+module.exports = validateRage;
